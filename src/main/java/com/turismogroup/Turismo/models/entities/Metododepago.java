8a90e8dd82273681b0ab34cbe5da1634c7a5a271
enoc1093 <10gambeta10@gmail.com>
2019-05-16 14:10:04 -0600

    entidades relacionales

--- src/main/java/com/turismogroup/Turismo/models/entities/Metododepago.java
+++ src/main/java/com/turismogroup/Turismo/models/entities/Metododepago.java
@@ -1,8 +1,13 @@
 package com.turismogroup.Turismo.models.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,9 +28,19 @@ public class Metododepago {
 		
 		private String Banco;
 		
-		private String DUI;
+		@ManyToOne(fetch=FetchType.LAZY)
+		@JoinColumn(name="DUI", nullable=true, unique = true)
+		@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+		private Ruta DUI;
 		
-		private String RutaID;
+		@ManyToOne(fetch=FetchType.LAZY)
+		@JoinColumn(name="RutaId", nullable=true, unique = true)
+		@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+		private Ruta RutaID;
 		
-		private String RutadetalleID;
+		@ManyToOne(fetch=FetchType.LAZY)
+		@JoinColumn(name="RutadetalleId", nullable=true, unique = true)
+		@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+		private Ruta RutadetalleID;
+		
 }
--- src/main/java/com/turismogroup/Turismo/models/entities/Ruta.java
+++ src/main/java/com/turismogroup/Turismo/models/entities/Ruta.java
@@ -1,8 +1,13 @@
 package com.turismogroup.Turismo.models.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +25,10 @@ public class Ruta {
 		
 		private String Kilometraje;
 		
-		private String ViajeID;
+		@ManyToOne(fetch=FetchType.LAZY)
+		@JoinColumn(name="ViajeID", nullable=true, unique = true)
+		@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+		private Ruta ViajeID;
 		
 		private String HoraSalida;
 		
@@ -30,7 +38,10 @@ public class Ruta {
 		
 		private String Disponibilidad;
 		
-		private String DUI;
+		@ManyToOne(fetch=FetchType.LAZY)
+		@JoinColumn(name="DUI", nullable=true, unique = true)
+		@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+		private Ruta DUI;
 		
 		private String Asiento_Personas;
 }
